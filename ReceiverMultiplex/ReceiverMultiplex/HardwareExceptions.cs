de703450ecd358addd1455fa5a0966f7d5977c9d
Shawn Bandy <shawn.bandy@thechain.net>
2012-10-08 14:34:22 -0700

    intermediate commit... Receiver stuff

---
+++ ReceiverMultiplex/ReceiverMultiplex/HardwareExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiverMultiplex
+{
+    class HardwareExceptions: Exception
+    {
+        private Boolean fatal {get; private set;}
+        private string text { get; private set; }
+
+        public HardwareExceptions(string text, Boolean fatal);
+    }
+}
--- ReceiverMultiplex/ReceiverMultiplex/Receivers.cs
+++ ReceiverMultiplex/ReceiverMultiplex/Receivers.cs
@@ -19,7 +19,7 @@ namespace ReceiverMultiplex
         private int TTL { get; private set; }
         private SerialPort serialPort { private get; private set; }
         private string portName {get; private set;}
-        
+        private dynamic receiverConfig { get; private set; }
         private int firmwareVersion {get; private set;}
         private string commandPreamble { get; private set; }
 
@@ -30,12 +30,29 @@ namespace ReceiverMultiplex
             this.serialPort = serialPort;
             this.portName = portName;
             this.dispatcher = dispatcher;
-            
+
+            int fw_ver = INFO();
+            if (fw_ver < 0)
+            {
+                serialPort.Close();
+                throw new HardwareExceptions("INFO command failed to return firmware version.", true);
+            }
+
+            int fw_use = -1;
             var jsonParser = new JsonParser(){CamelizeProperties = true};
             foreach(string filename in System.IO.Directory.GetFiles(VR2C_COMMAND_FOLDER))
             {
                 dynamic config = jsonParser.Parse(System.IO.File.ReadAllText(filename));
-                configFiles.Add(config.VersionNumber,filename);
+                if (config.FwVersion >= fw_use && config.FwVersion <= fw_ver)
+                {
+                    fw_use = config.FwVersion;
+                    receiverConfig = config;
+                }
+            }
+            if(fw_use < 0) 
+            {
+                serialPort.Close();
+                throw new HardwareExceptions("Valid json config not found for receiver firmware.",true);
             }
         }
         
@@ -48,7 +65,11 @@ namespace ReceiverMultiplex
             return 0;
         }
 
-        
+        private async Task<int> asyncRead()
+        {
+
+        }
+
 
     }
 }
