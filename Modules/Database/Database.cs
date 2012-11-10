1f2b9ce77ae54b429d1017f2919943b6ec309563
Mason Wright <Teltnuag@Gmail.com>
2012-11-09 21:12:53 -0800

    Incorporating new JSON file, sort of. Added some documentation.

--- Modules/Database/Database.cs
+++ Modules/Database/Database.cs
@@ -10,52 +10,168 @@ using System.Data;
 
 namespace Databases
 {
+    /// <summary>
+    /// This module handles the insertion of detections and status events into the database.
+    /// </summary>
     public class Database : Module
     {
+        private Dictionary<string, List<string>> sensor_calibrations { get; set; }
         private string connectionString {get; set;}
 
-        public Database(Dispatcher dispatcher, string host = "localhost", string db = "csulbsha_sharktopus", string user = "testuser", string pass = "testpass")
+        /// <summary>
+        /// The constructor for the Database module.
+        /// </summary>
+        /// <param name="dispatcher">The Dispatcher this object will receive messages from.</param>
+        /// <param name="transmitters">The JSON file containing the sensor tag calibrations.</param>
+        /// <param name="host">The host name of the database to connect to.</param>
+        /// <param name="db">The name of the database to connect to.</param>
+        /// <param name="user">The username of the database to connect to.</param>
+        /// <param name="pass">The password for the user.</param>
+        public Database(Dispatcher dispatcher, dynamic transmitters, string host = "localhost", string db = "csulbsha_sharktopus", string user = "testuser", string pass = "testpass")
         {
             connectionString = "Server=" + host + ";Database=" + db + ";Uid=" + user + ";Pwd=" + pass + ";";
+            updateSensorCalibrations(transmitters);
         }
 
+        /// <summary>
+        /// Returns the human readable name of this module.
+        /// </summary>
+        /// <returns>The name of this module.</returns>
         public override string getModuleName()
             { return "Database"; }
+
+        /// <summary>
+        /// Updates the list of of sensor tag calibrations from the JSON file. This should be called when the JSON file is updated.
+        /// </summary>
+        /// <param name="calibrations">The JSON file containing the calibration values.</param>
+        public void updateSensorCalibrations(dynamic calibrations)
+        {
+            this.sensor_calibrations = new Dictionary<string,List<string>>();
+            foreach (dynamic transmitter in calibrations.transmitters)
+            {
+
+            }
+        }
         
+        /// <summary>
+        /// The hook for the event dispatcher. Determines the type of message, and if applicable, makes a database insertion.
+        /// </summary>
+        /// <param name="realTimeEvent"></param>
         public override void onRealTimeEvent(RealTimeEvent realTimeEvent)
         {
+            int response = 0;
             if(realTimeEvent.GetType() == typeof(Decoder.RealTimeEvents.Decoded))
             {
                 Decoder.RealTimeEvents.Decoded rte = (Decoder.RealTimeEvents.Decoded)realTimeEvent;
                 string eventType = rte["messagetype"];
                 if (eventType == "detection_event")
-                    detectionInsert(rte);
+                    response = detectionInsert(rte);
+                else if (eventType == "status_response")
+                    response = statusInsert(rte);
+                if (response > 1)
+                    response = 1;
             }
-            else if(realTimeEvent.GetType() == typeof(ReceiverSlice.RealTimeEvents.NewReceiver))
-                receiverInsert((ReceiverSlice.RealTimeEvents.NewReceiver)realTimeEvent);
+            //return response;
         }
 
-        private void receiverInsert(ReceiverSlice.RealTimeEvents.NewReceiver newReceiver)
-        {
-            string statement = "INSERT INTO receivers (id) VALUES ('" + newReceiver["serialnumber"] + "');";
-            int response = doInsert(statement);
-            dispatcher.enqueueEvent(new Databases.RealTimeEvents.DatabaseResponse(statement, response, newReceiver));
-        }
+        // We are no longer responsible for populating the receivers table in the database with new receivers.
+        //private void receiverInsert(ReceiverSlice.RealTimeEvents.NewReceiver newReceiver)
+        //{
+        //    string statement = "INSERT INTO receivers (id) VALUES ('" + newReceiver["serialnumber"] + "');";
+        //    int response = doInsert(statement);
+        //    dispatcher.enqueueEvent(new Databases.RealTimeEvents.DatabaseResponse(statement, response, newReceiver));
+        //}
 
-        private void detectionInsert(Decoder.RealTimeEvents.Decoded detection)
+        /// <summary>
+        /// Makes an insertion into the vue table of the database for a detection event.
+        /// </summary>
+        /// <param name="detection">The RealTimeEvent object containing all necessary information from the detection.</param>
+        /// <returns>The number of rows affected by the insertion.</returns>
+        private int detectionInsert(Decoder.RealTimeEvents.Decoded detection)
         {
-            //string fullserialmodel = detection["model"] + ":" + detection["serialnumber"]
+            string date = detection["decodedmessage"]["date"];
+            string time = detection["decodedmessage"]["time"];
+            string frequency_codespace = detection["decodedmessage"]["frequency_codespace"];
+            string transmitter_id = detection["decodedmessage"]["transmitter_id"];
+            string receiver_model_id = detection["model"] + '-' + detection["serialnumber"];
+            string sensor_value = detection["decodedmessage"]["sensor_value"];
+            string sensor_type = "A2D";
             string statement;
-            if(detection["decodedmessage"]["sensor_value"] == null)
+            if (sensor_value == null)
                 statement = "INSERT INTO vue (date, time, frequency_codespace, transmitter_id, receivers_id) VALUES ('" +
-                        detection["decodedmessage"]["date"] + "', '" + detection["decodedmessage"]["time"] + "', '" + detection["decodedmessage"]["frequency_codespace"] + "', " + detection["decodedmessage"]["transmitter_id"] + ", '" + detection["decodedmessage"]["receivers_id"] + "');";
+                        date + "', '" + time + "', '" + frequency_codespace + "', " + transmitter_id + ", '" + receiver_model_id + "');";
             else
+            {
+                string transmitter_codespace_id = frequency_codespace + '-' + transmitter_id;
+                if (sensor_calibrations.ContainsKey(transmitter_codespace_id))
+                {
+                    sensor_type = sensor_calibrations[transmitter_codespace_id][0];
+                    sensor_value = getCalibratedSensorValue(sensor_type, double.Parse(sensor_value), double.Parse(sensor_calibrations[transmitter_codespace_id][1]), double.Parse(sensor_calibrations[transmitter_codespace_id][2])).ToString();
+                }
                 statement = "INSERT INTO vue (date, time, frequency_codespace, transmitter_id, sensor_value, sensor_unit, receivers_id) VALUES ('" +
-                        detection["decodedmessage"]["date"] + "', '" + detection["decodedmessage"]["time"] + "', '" + detection["decodedmessage"]["frequency_codespace"] + "', " + detection["decodedmessage"]["transmitter_id"] + ", " + detection["decodedmessage"]["sensor_value"] + ", 'm', '" + detection["decodedmessage"]["receivers_id"] + "');";
+                        date + "', '" + time + "', '" + frequency_codespace + "', " + transmitter_id + ", " + sensor_value + ", '" + sensor_type + "', '" + receiver_model_id + "');";
+            }
             int response = doInsert(statement);
             dispatcher.enqueueEvent(new Databases.RealTimeEvents.DatabaseResponse(statement, response, detection));
+            return response;
         }
 
+        /// <summary>
+        /// Returns the calibrated value for the A2D value given as well as the type of reading and the coefficients.
+        /// </summary>
+        /// <param name="sensor_type">The type of measurement, for example "p", "t", or "m".</param>
+        /// <param name="raw_value">The A2D value.</param>
+        /// <param name="a">The high-order coefficient.</param>
+        /// <param name="b">The low-order coefficient.</param>
+        /// <returns>The calibrated value.</returns>
+        private double getCalibratedSensorValue(string sensor_type, double raw_value, double a, double b)
+        {
+            double calibratedValue = raw_value;
+            sensor_type = sensor_type.ToLower();
+            if (sensor_type == "t" || sensor_type == "m") //linear -- ax + b
+                calibratedValue = a * raw_value + b;
+            else if (sensor_type == "p") //quadratic -- ax² + b
+                calibratedValue = a * raw_value * raw_value + b;
+            return calibratedValue;
+        }
+
+        /// <summary>
+        /// Makes an insertion into the receiver_status table of the databse for a status event.
+        /// </summary>
+        /// <param name="status">The RealTimeEvent object containing all necessary information from the status event.</param>
+        /// <returns>The number of rows affected by the insertion.</returns>
+        private int statusInsert(Decoder.RealTimeEvents.Decoded status)
+        {
+            string receiver_model_id = status["model"] + '-' + status["serialnumber"];
+            string date = status["decodedmessage"]["date"];
+            string time = status["decodedmessage"]["time"];
+            string dc = status["decodedmessage"]["dc"];
+            string pc = status["decodedmessage"]["pc"];
+            string lv = status["decodedmessage"]["lv"];
+            string bc = status["decodedmessage"]["bc"];
+            string bu = status["decodedmessage"]["bu"];
+            string i = status["decodedmessage"]["i"];
+            string t = status["decodedmessage"]["t"];
+            string du = status["decodedmessage"]["du"];
+            string ru = status["decodedmessage"]["ru"];
+            string xyz = status["decodedmessage"]["xyz"];
+            string statement;
+            if(xyz == null)
+                statement = "INSERT INTO receiver_status (id, date, time, detection_count, ping_count, line_voltage, battery_used, current, temperature, detection_memory, raw_memory) VALUES ('" +
+                        receiver_model_id + "', '" + date + "', '" + time + "', " + dc + ", " + pc + ", " + lv + ", " + bc + ", " + bu + ", " + i + ", " + t + ", " + du + ", " + ru + ");";
+            else
+                statement = "INSERT INTO receiver_status (id, date, time, detection_count, ping_count, line_voltage, battery_used, current, temperature, detection_memory, raw_memory, xyz_orientation) VALUES ('" +
+                        receiver_model_id + "', '" + date + "', '" + time + "', " + dc + ", " + pc + ", " + lv + ", " + bc + ", " + bu + ", " + i + ", " + t + ", " + du + ", " + ru + ", '" + xyz + "');";
+            int response = doInsert(statement);
+            dispatcher.enqueueEvent(new Databases.RealTimeEvents.DatabaseResponse(statement, response, status));
+            return response;
+        }
+
+        /// <summary>
+        /// Attempts to make the actual insertion into the database.
+        /// </summary>
+        /// <param name="statement">The SQL statement to be performed on the database.</param>
+        /// <returns>The number of rows affected by the insertion.</returns>
         private int doInsert(string statement)
         {
             int response = -1;
