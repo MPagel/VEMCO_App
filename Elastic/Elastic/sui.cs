47a0a35849aa953abd7a924a945f08e6530e65b5
Shawn Bandy <shawn.bandy@thechain.net>
2012-11-19 15:44:19 -0800

    intermediate 2

---
+++ Elastic/Elastic/sui.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elastic
+{
+    class sui : EventSlice.Interfaces.Module
+    {
+
+        public sui(EventSlice.Dispatcher d)
+            : base(d) { }
+
+
+        public override string getModuleName()
+        { return "Server User Interface"; }
+
+         /// <summary>
+        /// The hook for the event dispatcher.
+        /// </summary>
+        /// <param name="rte">realtime event</param>
+        public override void onRealTimeEvent(EventSlice.Interfaces.RealTimeEvent rte)
+        {
+
+        }
+
+
+    }
+}
