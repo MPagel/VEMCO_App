2bb5e51b8da891674c3a9410e363fe115ffe8273
Shawn Bandy <shawn.bandy@thechain.net>
2012-11-26 11:57:00 -0800

    design docs

--- Sandbox/Sandbox/ConsoleLogger.cs
+++ Sandbox/Sandbox/ConsoleLogger.cs
@@ -6,17 +6,29 @@ using System.Threading.Tasks;
 
 namespace Sandbox
 {
+    /// <summary>
+    /// This module sends the text of any real time event to the conole.
+    /// </summary>
     public class ConsoleLogger : Module
     {
+
 
         public ConsoleLogger(Dispatcher dispatcher)
             : base(dispatcher) { }
 
+        /// <summary>
+        /// The event dispatcher's hook.
+        /// </summary>
+        /// <param name="rte">The real time event to be acted upon by this module.</param>
         public override void onRealTimeEvent(RealTimeEvent rte)
         {
             System.Console.WriteLine(rte);
         }
         
+        /// <summary>
+        /// For use by UI code to get a display name for this module.
+        /// </summary>
+        /// <returns>Human-readable name of this module</returns>
         public override string getModuleName()
         {
             return "ConsoleLogger";
